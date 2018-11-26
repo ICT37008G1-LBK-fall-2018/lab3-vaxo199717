405eb0c51e6c238c831f4cd4c2014089f70606b9
vaxo ozgebishvili <vaxoozgebishvili.27@gmail.com>
2018-11-26 07:18:28 -0800

    L308

---
+++ L308/script.js
@@ -0,0 +1,4 @@
+let student_name = prompt("shetainet studentebis saxelebi").split(' ');
+console.log('student_name: ', student_name);
+var reversed = student_name.reverse(); 
+console.log('reversed: ', reversed);
