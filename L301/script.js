e04e964f45eeb9ed813df46922f50fb04d8124e5
vaxo ozgebishvili <vaxoozgebishvili.27@gmail.com>
2018-11-26 07:05:19 -0800

    L301

---
+++ L301/script.js
@@ -0,0 +1,12 @@
+
+let p = prompt("Enter String");
+if (p.match("reklama")) {
+    alert("akrdaluli sityva: reklama !!");
+}
+if (p.match("marketingi")) {
+    alert("akrdzaluli sityva: marketingi !!");
+}
+if (p.match("virusi")) {
+    alert("akrdzaluli sityva: virusi");
+}
+alert("You Entered: " + p);
