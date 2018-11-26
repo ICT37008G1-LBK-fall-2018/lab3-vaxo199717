767400de2df010c718a8c9ebd044a5cfa15a9220
vaxo ozgebishvili <vaxoozgebishvili.27@gmail.com>
2018-11-26 07:10:17 -0800

    L304

---
+++ L304/script.js
@@ -0,0 +1,13 @@
+let student_amount = +prompt("Select Amount of Student");
+let student_name = ['vaxo', 'lasha', 'oto', 'sergi', 'robi'];
+if (student_amount > student_name.length || student_amount <= 0) {
+ alert("arasworia, sheitanet 10ze naklebi");
+}    
+
+else{
+    for (let i = 0; i < student_amount; i++) {
+        alert(student_name[i]);
+
+
+    }
+}
