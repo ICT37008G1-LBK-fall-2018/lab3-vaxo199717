48589a8b682b86e234e7650e3564c85b9c7f7ebc
vaxo ozgebishvili <vaxoozgebishvili.27@gmail.com>
2018-11-26 07:16:18 -0800

    L307

---
+++ L307/script.js
@@ -0,0 +1,7 @@
+
+var arr = [];                               
+for (var i = 0; i < 10; i++) {              
+    arr.push(prompt('Enter grade ' + (i + 1))); 
+}
+
+alert('Full array: ' + arr.join(', '));    
